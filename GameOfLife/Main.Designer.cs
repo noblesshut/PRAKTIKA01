4d6ebdbf5513b4ace07d19464e1eb38a6d9c01ee
noblesshut <136069615+noblesshut@users.noreply.github.com>
2024-04-01 23:37:57 +0300

    Add files via upload

---
+++ GameOfLife/Main.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace GameOfLife
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.Exit = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Stop = new System.Windows.Forms.Button();
+            this.Start = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.Exit);
+            this.splitContainer1.Panel1.Controls.Add(this.Clear);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.Stop);
+            this.splitContainer1.Panel1.Controls.Add(this.Start);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(1273, 638);
+            this.splitContainer1.SplitterDistance = 202;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // Exit
+            // 
+            this.Exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.Exit.Location = new System.Drawing.Point(27, 345);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(133, 48);
+            this.Exit.TabIndex = 8;
+            this.Exit.Text = "Выход";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click_1);
+            // 
+            // Clear
+            // 
+            this.Clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.Clear.Location = new System.Drawing.Point(27, 276);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(133, 49);
+            this.Clear.TabIndex = 7;
+            this.Clear.Text = "Очистить поле\r\n";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Игра \"Жизнь\"";
+            // 
+            // Stop
+            // 
+            this.Stop.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Stop.Location = new System.Drawing.Point(27, 206);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(133, 50);
+            this.Stop.TabIndex = 5;
+            this.Stop.Text = "Стоп";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Start
+            // 
+            this.Start.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Start.Location = new System.Drawing.Point(27, 134);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(133, 49);
+            this.Start.TabIndex = 4;
+            this.Start.Text = "Старт";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1063, 634);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 40;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1273, 638);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Main";
+            this.Text = "Игра \"Жизнь\"";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Resize += new System.EventHandler(this.Main_Resize);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Exit;
+    }
+}
+
---
+++ GameOfLife/Main.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameOfLife
+{
+    public partial class Main : Form
+    {
+        private Graphics graphics;// переменная для отрисовки клеток
+        private int resolution = 10;// размерность клеток
+        private bool[,] field;// массив для красных клеток
+        private bool[,] field2;// массив для синих клеток
+        private int cols; // кол-во столбцов
+        private int rows;// кол-во строк 
+        public Main()
+        {
+            InitializeComponent();
+            Main_Resize(null, null);  //изменение размеров поля для игры относительно размеров окна Windows Form 
+        }
+        private void StartGame() // ф-ия начала игры
+        {
+            if (timer1.Enabled) // если таймер включен и идет появление след. поколений, то ничего не делаем
+            {
+                return; // выход из данной ф-ии
+            }
+            timer1.Start(); // запускаем таймер
+        }
+        private void NextGeneration() // вычисление и отрисовка поколений клеток
+        {
+            graphics.Clear(Color.White); // чистим игровое поле для отрисовки след. поколения
+            var NewField = new bool[cols,rows];//массив для красных клеток
+            var NewField2 = new bool[cols, rows];//массив для синих клеток
+            for (int x = 0; x < cols; x++)
+            {// циклы для прохода по всем клеткам массива
+                for (int y = 0; y < rows; y++)
+                {
+                    var neighboursCount = CountNeighbours(x, y, field); // нахождение соседей к красной клетки прошлого поколения
+                    var hasLife = field[x, y]; // переменная, для определения (жива ли красная клетка)
+
+                    if (!hasLife && neighboursCount == 3) // если клетка не живая и рядом 3 соседки
+                        NewField[x, y] = true; // клетка оживает
+
+                    else if (hasLife && (neighboursCount < 2 || neighboursCount > 3)) // иначе если клетка жива и кол-во соседей меньше 2 или больше 3
+                        NewField[x, y] = false;// клетка умирает
+                    else
+                        NewField[x, y] = field[x, y]; // иначе координаты клетки не изменяются
+                    if (hasLife)// если клетка жива
+                        graphics.FillRectangle(Brushes.Red, x * resolution, y * resolution, resolution - 1, resolution - 1); // то она красная
+                }
+            }
+            for (int x = 0; x < cols; x++) 
+            {// цикы для прохода по всем клеткам массива
+                for (int y = 0; y < rows; y++)
+                {
+                    var neighboursCount = CountNeighbours2(x, y,field2); // нахождение соседей к синей клетке прошлого поколения
+                    var hasLife = field2[x, y];// переменная, для определения (жива ли синяя клетка)
+
+                    if (!hasLife && neighboursCount == 3)// если клетка не живая и рядом 3 соседки
+                        NewField2[x, y] = true;//клетка оживает
+
+                    else if (hasLife && (neighboursCount < 2 || neighboursCount > 3))// иначе если клетка жива и кол-во соседей меньше 2 или больше 3
+                        NewField2[x, y] = false;// клетка умирает
+                    else
+                        NewField2[x, y] = field2[x, y];// иначе координаты клетки не изменяются
+                    if (hasLife)// если клетка жва
+                        graphics.FillRectangle(Brushes.Blue, x * resolution, y * resolution, resolution - 1, resolution - 1);// то она синяя
+                }
+            }
+            field = NewField;// новое поколение красных клеток становится старым
+            field2 = NewField2;//новое поколение синих клеток становится текущим
+            pictureBox1.Refresh();// отрисовываем новое поколение
+        }
+        private int CountNeighbours(int x, int y,bool [,]A)// считаем кол-во соседей у красной клетки
+        {
+            int count = 0;// считаем кол-во соседей у клетки
+            for (int i = -1; i < 2; i++)
+            {// считаем кол-во соседей у клетки
+                for (int j = -1;j < 2; j++)
+                {
+                    var col = (x + i + cols) % cols; // нахождение соседних столбцов
+                    var row = (y + j + rows) % rows;// нахождение соседних строк
+                    var isSelfChecking = col == x && row == y; // самопроверка
+                    bool hasLife = A[col, row];// переменная, проверяющая есть жизнь или нет
+                    if (hasLife &&  !isSelfChecking)// если клетка жива и не сампроверяется
+                    {
+                        count++;// записываем соседа
+                    }
+                }
+            }
+            return count;
+        }
+
+        private int CountNeighbours2(int x, int y, bool[,] B)// считаем кол-во соседей у синей клетки
+        {
+            int count = 0;// считаем кол-во соседей у клетки
+            for (int i = -1; i < 2; i++)
+            {// считаем кол-во соседей у клетки
+                for (int j = -1; j < 2; j++)
+                {
+                    var col = (x + i + cols) % cols;// нахождение соседних столбцов
+                    var row = (y + j + rows) % rows;// нахождение соседних строк
+                    var isSelfChecking = col == x && row == y;// самопроверка
+                    bool hasLife = B[col, row];// переменная, проверяющая есть жизнь или нет
+                    if (hasLife && !isSelfChecking)// если клетка жива и не самопровеяется
+                    {
+                        count++;// добавляем соседа
+                    }
+                }
+            }
+            return count;
+        }
+        private void StopGame() // останавливаем игру
+        {
+            if (!timer1.Enabled) // если таймер не включен
+            {
+                return; // выходим из данной ф-ии
+            }
+            timer1.Stop(); // останавливаем таймер
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) // таймер
+        {
+            NextGeneration(); // вызов ф-ии генерации след.поколения
+        }
+        private void Clear_Click_1(object sender, EventArgs e) // ф-ия очистки игрового поля
+        {
+            timer1.Enabled = false;  //остановка таймера(а также генерации следущего поколения)
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);  //создание растрового изображения
+            graphics = Graphics.FromImage(pictureBox1.Image);  //инициализация переменной graphics растровым изображением
+            graphics.Clear(Color.White);  //заполнение поля белым цветом
+
+            for (int i = 0; i < cols; i++)  //цикл, который проходится по всем элементам массива с красными и синими клетками 
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    field[i, j] = false; //и удаляет их с поля
+                    field2[i, j] = false; //и удаляет их с поля
+                }
+            }
+        }
+        private void Start_Click(object sender, EventArgs e) // ф-ия запускает игру
+        {
+            StartGame(); // запуск игры
+        }
+
+        private void Stop_Click(object sender, EventArgs e) // ф-ия останавливает игру
+        {
+            StopGame(); // остановка игры
+        }
+        private bool Granica(int x, int y) // ф-ия для проверки границ рисования клеток
+        {
+            return x >= 0 && y >= 0 && x < cols && y < rows; // если координаты(при нажатии кнопкой),входят в состав индексов массива, то все ок
+        }
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e) // В данном методе мы выполняем отрисовку клеток разных цветов по нажатию на поле
+        {
+            if (timer1.Enabled)  //таймер включён -> запрещаем рисовать клетки
+                return; //выходим из данной функции
+
+            int x = e.Location.X / resolution;  //координата по x в массиве
+            int y = e.Location.Y / resolution;  //координата по y в массиве
+
+            if (e.Button == MouseButtons.Left)  //если нажата ЛКМ
+            {
+                if (Granica(x, y))  //и если координаты клетки в диапазоне индексов массива
+                {
+                    if (field[x, y])  //и если клетка уже существует в данной координате
+                    {
+                        field[x, y] = false;  //то, стираем клетку из массива и отрисовываем её белым цветом
+                        graphics.FillRectangle(Brushes.White, x * resolution, y * resolution, resolution - 1, resolution - 1);
+                    }
+                    else  //если клетки ещё не существует в данных координатах
+                    {
+                        field[x, y] = true;  //то, создаём клетку в данных координатах и отрисовываем её красным цветом
+                        graphics.FillRectangle(Brushes.Red, x * resolution, y * resolution, resolution - 1, resolution - 1);
+                    }
+                }
+            }
+            if (e.Button == MouseButtons.Right)  //если нажата ПКМ
+            {
+                if (Granica(x, y))  //если координата клетки в диапазоне индексов массива
+                {
+                    if (field2[x, y])  //если клетка уже существует в данной координате
+                    {
+                        field2[x, y] = false;  //стираем клетку из массива и отрисовываем её белым цветом
+                        graphics.FillRectangle(Brushes.White, x * resolution, y * resolution, resolution - 1, resolution - 1);
+                    }
+                    else  //если клетки ещё не существует в данных координатах
+                    {
+                        field2[x, y] = true;  //создаём клетку в данных координатах и отрисовываем её синим цветом
+                        graphics.FillRectangle(Brushes.Blue, x * resolution, y * resolution, resolution - 1, resolution - 1);
+                    }
+                }
+            }
+            pictureBox1.Refresh();  //обновляем pictureBox в который внесены изменения с новыми клетками
+        }
+        private void Exit_Click_1(object sender, EventArgs e) // мтетод выхода из окна
+        {
+            Application.Exit(); // закрытие окна
+        }
+
+        private void Main_Resize(object sender, EventArgs e)//ф-ия для вычисления размерности формы
+        {
+            rows = pictureBox1.Height / resolution;  //вычисляем размерность экрана(кол-во строк и столбцов)
+            cols = pictureBox1.Width / resolution; //делим кол-во пикселей экрана на кол-во пикселей 1 клетки
+            // инициализируем размерность массивов 
+            field = new bool[cols, rows];
+            field2 = new bool[cols, rows];
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height); //создаём растровое изображение в pictureBox
+            graphics = Graphics.FromImage(pictureBox1.Image);  //переносим пустое растровое изображение в переменную графики(чтобы потом рисовать там)
+            graphics.Clear(Color.White);  //заполняем графику белым цветом
+        }
+    }
+}
